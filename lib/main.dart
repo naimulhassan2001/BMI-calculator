c3eea60968450274a14120a33d556fc4f64b295f
Naimul Hassan <nayem20012@gmail.com>
2023-10-31 22:10:51 +0600

    final project

--- lib/main.dart
+++ lib/main.dart
@@ -30,103 +30,111 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: TextField(
-                controller: age,
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextField(
+                    controller: age,
 
-                decoration: InputDecoration(
-                    labelText: "Age",
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20))),
-              ),
+                    decoration: InputDecoration(
+                        labelText: "Age",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                  ),
+                ),
+                const SizedBox(height: 16,),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextField(
+                    controller: weight,
+                    decoration: InputDecoration(
+                        labelText: "Weight",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                  ),
+                ),
+                const SizedBox(height: 16,),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextField(
+                    controller: feet,
+                    decoration: InputDecoration(
+                        labelText: "feet",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                  ),
+                ),
+                const SizedBox(height: 16,),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextField(
+                    controller: inch,
+                    decoration: InputDecoration(
+                        labelText: "inch",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                  ),
+                ),
+                const SizedBox(height: 16,),
+
+                ElevatedButton(onPressed: (){
+
+                  setState(() {
+                    var iAge = int.parse(age.text);
+                    var iWeight = int.parse(weight.text);
+                    var iFeet = int.parse(feet.text);
+                    var iInch = int.parse(inch.text);
+
+                    var dFeet = iFeet*30.48;
+                    var dinch = iInch * 2.54 ;
+
+                    var hight = dFeet+dinch ;
+                    var dhight = hight/100;
+
+                    var qhight = dhight*dhight ;
+
+
+                    var bm = iWeight/qhight ;
+
+                    if(30 < bm) {
+                      result = "Obesity";
+                      stats = "try to decrease your Over weight" ;
+                    } else if(bm > 25) {
+                      result = "Overweight " ;
+                      stats = "try to decrease your weight" ;
+                    } else if(bm > 18.5) {
+                      result = "Normal weight " ;
+                      stats = "your are perfect now" ;
+                    } else {
+                      result = "Underweight" ;
+                      stats = "try to increase your weight" ;
+                    }
+
+                    bmi = bm ;
+
+
+                  });
+
+                }, child: const Text("BMi")),
+                const SizedBox(height: 16,),
+                Text('$bmi', style: const TextStyle(fontSize: 26),),
+                const SizedBox(height: 16, ),
+                Text('$result', style: const TextStyle(fontSize: 26),),
+                const SizedBox(height: 10, ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Text('$stats', style: const TextStyle(fontSize: 26),),
+                ),
+
+
+              ],
             ),
-            const SizedBox(height: 16,),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: TextField(
-                controller: weight,
-                decoration: InputDecoration(
-                    labelText: "Weight",
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20))),
-              ),
-            ),
-            const SizedBox(height: 16,),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: TextField(
-                controller: feet,
-                decoration: InputDecoration(
-                    labelText: "feet",
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20))),
-              ),
-            ),
-            const SizedBox(height: 16,),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: TextField(
-                controller: inch,
-                decoration: InputDecoration(
-                    labelText: "inch",
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20))),
-              ),
-            ),
-            const SizedBox(height: 16,),
-
-            ElevatedButton(onPressed: (){
-
-              setState(() {
-                var iAge = int.parse(age.text);
-                var iWeight = int.parse(weight.text);
-                var iFeet = int.parse(feet.text);
-                var iInch = int.parse(inch.text);
-
-                var dFeet = iFeet*30.48;
-                var dinch = iInch * 2.54 ;
-
-                var hight = dFeet+dinch ;
-                var dhight = hight/100;
-
-                var qhight = dhight*dhight ;
-
-
-                var bm = iWeight/qhight ;
-
-                if(30 < bm) {
-                  result = "Obesity";
-                  stats = "try to decrease your Overweight" ;
-                } else if(bm > 25) {
-                  result = "Overweight " ;
-                  stats = "try to decrease your weight" ;
-                } else if(bm > 18.5) {
-                  result = "Normal weight " ;
-                  stats = "your are perfect now" ;
-                } else {
-                  result = "Underweight" ;
-                  stats = "try to increase your weight" ;
-                }
-
-                bmi = bm ;
-
-
-              });
-
-            }, child: const Text("BMi")),
-            const SizedBox(height: 16,),
-            Text('$bmi', style: const TextStyle(fontSize: 26),),
-            const SizedBox(height: 16, ),
-            Text('$result', style: const TextStyle(fontSize: 26),),
-            Text('$stats', style: const TextStyle(fontSize: 26),),
-
-
-          ],
+          ),
         ),
       ),
     );
